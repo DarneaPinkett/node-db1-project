38c4a23a1f938141809c9addf528db72804f90f4
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-04-24 11:08:34 -0400

    sql, mvp, stretch

--- acctRoutes.js
+++ acctRoutes.js
@@ -2,6 +2,18 @@ const express = require('express');
 const db = require('./data/dbConfig');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.select('*')
+    .from('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({error: "Could not get the all accounts"});
+    })
+})
+
 router.post('/', (req, res) => {
     db('accounts')
     .insert(req.body, 'id')
@@ -42,3 +54,5 @@ router.delete('/:id', (req, res) => {
         res.status(500).json({error: "Could not delete account"});
     })
 })
+
+module.exports = router;
--- queries.sql
+++ queries.sql
@@ -1,17 +1,36 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers
+where postalcode = '10101';
 
 -- Find the phone number for the supplier with the id 11
+SELECT phone FROM suppliers
+where SupplierId = '11';
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT * FROM orders
+order by orderdate DESC
+limit 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM Customers
+where City in ('London', 'Madrid')
+or Country = 'Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO Customers
+(customerName, contactName, [address], City, postalCode, Country)
+values('The Shire', 'Bilbo Baggines', '1 hobbit-hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers set PostalCode = 11122
+WHERE ContactName = 'Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT DISTINCT City
+FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT * FROM Suppliers
+WHERE Length(SupplierName) > 20;
