91ef3a18ad72413d57675c74815a4d83e584b1dd
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-04-24 09:51:59 -0400

    server

--- api/server.js
+++ api/server.js
@@ -4,6 +4,14 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
+const AcctRouter = require('../acctRoutes');
+
 server.use(express.json());
+
+server.use('/acctRoutes.js', AcctRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h1>Helpers with knex</h1>')
+});
 
 module.exports = server;
